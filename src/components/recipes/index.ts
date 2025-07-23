c8cde4f727a131a298bca8187baea3a5886b2004
Padu76 <andrea.padoan@gmail.com>
2025-07-23 22:08:09 +0200

    Update index.ts

--- src/components/recipes/index.ts
+++ src/components/recipes/index.ts
@@ -1,1 +1,12 @@
+// 📱 Scanner & Tools
+export { default as BarcodeScanner } from './BarcodeScanner';
 
+// 🔍 Future components will be exported here
+// export { default as RecipeSearch } from './RecipeSearch';
+// export { default as RecipeFilters } from './RecipeFilters';
+// export { default as FavoriteIngredients } from './FavoriteIngredients';
+// export { default as RecipeCard } from './RecipeCard';
+// export { default as RecipeModal } from './RecipeModal';
+// export { default as RecipeDatabase } from './RecipeDatabase';
+// export { default as NutritionFacts } from './NutritionFacts';
+// export { default as ExportActions } from './ExportActions';
